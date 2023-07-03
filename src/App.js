e677425644d26bbde56c52ff34b42d245df14e0f
Nikhil Tiwary <ntiwary61@gmail.com>
2023-07-03 10:51:39 +0530

    add NewItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import NewItem from "./components/NewItem";
 
 function App() {
   return (
     <div className="App">
       <>
         <h1>To-Do List</h1>
+        <NewItem></NewItem>
       </>
     </div>
   );
---
+++ src/components/NewItem.js
@@ -0,0 +1,10 @@
+import "./NewItem.css";
+
+export default function NewItem() {
+  return (
+    <div className="task">
+      <h3>TASK</h3>
+      <p>Description</p>
+    </div>
+  );
+}
