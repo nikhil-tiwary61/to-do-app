97aa7091425a02369434f58cc9a2763f67a2d9e9
Nikhil Tiwary <ntiwary61@gmail.com>
2023-07-03 10:57:27 +0530

    add Form component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Form from "./components/Form";
 import NewItem from "./components/NewItem";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <>
         <h1>To-Do List</h1>
+        <Form></Form>
         <NewItem></NewItem>
       </>
     </div>
---
+++ src/components/Form.js
@@ -0,0 +1,9 @@
+export default function Form() {
+  return (
+    <div className="form">
+      <input type="text" placeholder="Title" />
+      <input type="text" placeholder="Description" />
+      <button>Add</button>
+    </div>
+  );
+}
